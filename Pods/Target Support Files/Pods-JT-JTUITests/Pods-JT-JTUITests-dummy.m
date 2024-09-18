aaeec41198dc54e82c888ed2cf3077e9fd496c21
zain <zain.ajmal@vintegasolutions.com>
2024-09-18 22:36:38 +0500

    Revert "git add ."
    This reverts commit 6b6f759f8235a6ff216929afac4ed2963df56f5b.

--- Pods/Target Support Files/Pods-JT-JTUITests/Pods-JT-JTUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JT_JTUITests : NSObject
-@end
-@implementation PodsDummy_Pods_JT_JTUITests
-@end
--- Pods/Target Support Files/Pods-JT/Pods-JT-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JT : NSObject
-@end
-@implementation PodsDummy_Pods_JT
-@end
